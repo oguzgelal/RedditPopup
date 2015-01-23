b8a462ed49766e796e378793471bb29f128866b8
Devin Rousso <dcrousso@gmail.com>
2015-01-22 18:15:08 -0800

    Removed necessity for jQuery

--- inject.js
+++ inject.js
@@ -1,44 +1,62 @@
-var html = " \
-<div class='popupFrame'> \
-<div class='frameContainer'> \
-<iframe src='http://oguzgelal.com/redditpopup/loading.html'></iframe> \
-</div> \
-<div class='buttonContainer'> \
-<div class='browseButton'>New Tab</div> \
-<div class='closeButton'>Close</div> \
-</div>\
-</div>";
+var popupFrame = document.createElement("div");
+popupFrame.id = "popupFrame";
 
-$(document).ready(function(){
-	$('body').append(html);	
-  $('.popupFrame .frameContainer iframe').prop('src', 'http://oguzgelal.com/redditpopup/loading.html');
-});
+var frameContainer = document.createElement("div");
+frameContainer.id = "frameContainer";
+popupFrame.appendChild(frameContainer);
 
-$(document).on('click', 'a.title', function(e){
-	e.preventDefault();
-	var url = $(this).prop('href');
-  if (e.ctrlKey || e.metaKey){ window.open(url, '_new'); }
-  else { iframeOpen(url); }
+var loadingSRC = chrome.extension.getURL("loading.html");
+var iframe = document.createElement("iframe");
+iframe.src = loadingSRC;
+frameContainer.appendChild(iframe);
 
-});
+var buttonContainer = document.createElement("div");
+buttonContainer.id = "buttonContainer";
+popupFrame.appendChild(buttonContainer);
 
-$(document).on('click', '.closeButton', function(){
+var browseButton = document.createElement("div");
+browseButton.id = "browseButton";
+browseButton.innerHTML = "New Tab";
+buttonContainer.appendChild(browseButton);
+
+var closeButton = document.createElement("div");
+closeButton.id = "closeButton";
+closeButton.innerHTML = "Close";
+buttonContainer.appendChild(closeButton);
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(popupFrame);
+
+var links = document.querySelectorAll("a.title");
+for(var i = 0; i < links.length; i++) {
+	links[i].addEventListener("click", function(e) {
+		e.preventDefault();
+		var url = this.href;
+		if(e.ctrlKey || e.metaKey){
+			window.open(url, '_new');
+		} else {
+			iframeOpen(url);
+		}
+		return false;
+	})
+}
+
+closeButton.addEventListener("mouseup", function(){
 	iframeClose();
 });
 
-$(document).on('click', '.browseButton', function(){
-	var src = $('.popupFrame .frameContainer iframe').prop('src');
-	window.open(src, '_new');
+browseButton.addEventListener("mouseup", function(){
+	window.open(iframe.src, '_new');
 	iframeClose();
 });
 
-$(document).keyup(function(e) {
+document.addEventListener("keyup", function(e) {
   if (e.keyCode == 27) {
   	iframeClose();
   }
 });
 
-$(document).on('click', '.popupFrame:not(.frameContainer)', function(){
+document.querySelector("#popupFrame:not(#frameContainer)").addEventListener("mouseup", function(){
   iframeClose();
 });
 
@@ -46,30 +64,30 @@ $(document).on('click', '.popupFrame:not(.frameContainer)', function(){
 
 // Open up iframe
 function iframeOpen(url){
-  if (!allowedUrl(url)){ window.open(url, '_new'); }
-  else {
-    $('.popupFrame .frameContainer iframe').prop('src', 'http://oguzgelal.com/redditpopup/loading.html');
+  if (!allowedUrl(url)){
+     window.open(url, '_new');
+  } else {
     // Disable page scroll
-    $('body').css('overflow', 'hidden');
-    $('body').css('height', '100%');
+    body.style.overflow = "hidden";
+    body.style.height = "100%";
     // Set iframe src and open popup
-    $('.popupFrame .frameContainer iframe').prop('src', url);
-    $('.popupFrame').fadeIn('fast');  
+    iframe.src = url;
+    popupFrame.className = "fadeIn";
   }
 }
 
 // Close iframe
 function iframeClose(){
   // Enable page scroll
-  $('body').css('overflow', 'auto');
-  $('body').css('height', 'auto');
+  body.style.overflow = "auto";
+  body.style.height = "100%";
   // Close popup
-  $('.popupFrame').hide();
-  $('.popupFrame .frameContainer iframe').prop('src', 'http://oguzgelal.com/redditpopup/loading.html');
+  popupFrame.className = "";
+  iframe.src = loadingSRC;
 }
 
+// iFrames do not work on X-Frame-Options SAMEORIGIN sites
 function allowedUrl(url){
-  var allowed = true;
   var notallowed = [
   "youtube.com",
   "youtu.be",
@@ -79,10 +97,10 @@ function allowedUrl(url){
   for(var i = 0; i < notallowed.length; i++){
     var match = url.match(new RegExp(notallowed[i], 'gi'));
     if (match && match.length > 0){
-      allowed = false;
+      return false;
     }
   }
-  return allowed;
+  return true;
 }
 
 
